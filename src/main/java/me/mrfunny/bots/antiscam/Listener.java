59a082f5d72a1ce5f43a6573943edb20977196e5
Sasha Semenishchev <sashasemenishchev@gmail.com>
2021-08-05 12:59:12 +0300

    Major bug fix

--- src/main/java/me/mrfunny/bots/antiscam/Listener.java
+++ src/main/java/me/mrfunny/bots/antiscam/Listener.java
@@ -63,17 +63,16 @@ public class Listener extends ListenerAdapter {
         ArrayList<Permission> permissions = new ArrayList<>();
         permissions.add(Permission.VIEW_CHANNEL);
         try {
-            Category category = guild.createCategory("AntiScam").addRolePermissionOverride(guild.getPublicRole().getIdLong(), new ArrayList<>(), permissions).complete();
-            List<TextChannel> possibleUpdatesChannels = guild.getTextChannelsByName("antiscam-updates", true);
-            List<TextChannel> possibleLogsChannels = guild.getTextChannelsByName("logs", true);
-            TextChannel updatesChannel = (possibleUpdatesChannels.isEmpty() ? category.createTextChannel("antiscam-updates").addMemberPermissionOverride(AntiScam.jda.getSelfUser().getIdLong(), permissions, new ArrayList<>()).addRolePermissionOverride(guild.getPublicRole().getIdLong(), new ArrayList<>(), permissions).complete() : possibleUpdatesChannels.get(0));
-            TextChannel logsChannel = (possibleLogsChannels.isEmpty() ? category.createTextChannel("logs").addMemberPermissionOverride(AntiScam.jda.getSelfUser().getIdLong(), permissions, new ArrayList<>()).addRolePermissionOverride(guild.getPublicRole().getIdLong(), new ArrayList<>(), permissions).complete() : possibleLogsChannels.get(0));
+            Category category = guild.createCategory("AntiScam").addMemberPermissionOverride(AntiScam.jda.getSelfUser().getIdLong(), permissions, new ArrayList<>()).addRolePermissionOverride(guild.getPublicRole().getIdLong(), new ArrayList<>(), permissions).complete();
+            TextChannel updatesChannel = category.createTextChannel("antiscam-updates").addMemberPermissionOverride(AntiScam.jda.getSelfUser().getIdLong(), permissions, new ArrayList<>()).addRolePermissionOverride(guild.getPublicRole().getIdLong(), new ArrayList<>(), permissions).complete();
+            TextChannel logsChannel = (category.createTextChannel("logs").addMemberPermissionOverride(AntiScam.jda.getSelfUser().getIdLong(), permissions, new ArrayList<>()).addRolePermissionOverride(guild.getPublicRole().getIdLong(), new ArrayList<>(), permissions).complete());
             logsChannel.sendMessage("Thanks for adding me! My current prefix is a!").queue(message -> sendHelp(logsChannel, "a!"));
             if(collection.find(new Document("server_id", guild.getId())).first() == null){
                 collection.insertOne(new Document("server_id", guild.getId()).append("logs_channel_id", logsChannel.getId()).append("prefix", "a!").append("updates_channel_id", updatesChannel.getId()));
             }
         } catch (Exception exception){
             try {
+                System.out.println(guild.getName());
                 exception.printStackTrace();
             } catch (InsufficientPermissionException exception1){
                 guild.leave().queue();
