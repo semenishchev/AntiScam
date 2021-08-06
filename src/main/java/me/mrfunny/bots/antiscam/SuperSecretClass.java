5127ffa736ae0b7eacdd3ce76ad52c04dea1ddc5
Sasha Semenishchev <sashasemenishchev@gmail.com>
2021-08-06 13:54:31 +0300

    Update config vars

--- src/main/java/me/mrfunny/bots/antiscam/SuperSecretClass.java
+++ src/main/java/me/mrfunny/bots/antiscam/SuperSecretClass.java
@@ -3,8 +3,8 @@ package me.mrfunny.bots.antiscam;
 import com.mongodb.ConnectionString;
 
 public class SuperSecretClass {
-    public static final String token = "ODcxMjkxMDgxMjk0MjMzNjYw.YQZLDQ.rFqQAYCz9ZyL_GPgBY30mNi_6cQ";
-    public static final String debugToken = "ODcyMzYzODEyNDIwOTgwNzY2.YQoyHA.HMpyvWrklfEAwEIM904PT4cWqyo";
-    public static final ConnectionString connectionString = new ConnectionString("mongodb+srv://MrFunny:12341234@cluster0.hxn1g.mongodb.net/antiscam?retryWrites=true&w=majority");
-    public static final String webhookUrl = "https://discord.com/api/webhooks/872435330006024193/z6ujM6r9QgdaKpLAL3jYUn-O4Iwmj6p6O-YG6rDvnzMz8jpZciM1W5KIknzMoJoDfOSx";
+    public static final String token = System.getenv().get("token");
+    public static final String debugToken = System.getenv().get("debugToken");
+    public static final ConnectionString connectionString = new ConnectionString(System.getenv().get("connectionString"));
+    public static final String webhookUrl = System.getenv().get("webhookUrl");
 }
